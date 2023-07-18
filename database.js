73cad090d351543773c28bde9022b935fb48ae52
Storm-Julian Tersteeg (student) <s1155443@student.windesheim.nl>
2023-07-18 12:02:12 +0900

    added more test content

--- database.js
+++ database.js
@@ -24,5 +24,39 @@ var database = [
         content: "This is the vegan food tip content"
       },
     ]
-  }
+  },
+  {
+    category: "Transportation",
+    icon: "directions_bus",
+    color: "#ff9f5b",
+    tips: [
+      {
+        title: "Biking",
+        content: "This is the biking tip content"
+      },
+      {
+        title: "Public<br>Transportation",
+        content: "This is the public transportation tip content"
+      },
+      {
+        title: "Electric Cars",
+        content: "This is the electric cars tip content"
+      },
+    ]
+  },
+  {
+    category: "Energy",
+    icon: "flash_on",
+    color: "#ffda5b",
+    tips: [
+      {
+        title: "Solar Panels",
+        content: "This is the solar panels tip content"
+      },
+      {
+        title: "Wind Turbines",
+        content: "This is the wind turbines tip content"
+      },
+    ]
+  },
 ]
