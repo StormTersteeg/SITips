4f5c915287c28bd92f81f6a7f6a2143703e33316
Storm-Julian Tersteeg (student) <s1155443@student.windesheim.nl>
2023-07-18 11:47:56 +0900

    improved tip showing

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function loadCategories() {
         ontouchstart="loadCategory(${i})"
       >
         ${category}
-        <span class="material-icons ml">${icon}</span>
+        <span class="material-icons ml-1">${icon}</span>
       </tile>
     `;
   }
@@ -59,6 +59,25 @@ function loadCategory(index) {
   target.innerHTML = html_string;
   toggleBackButton(true);
   setBackButtonTarget("loadCategories()");
+}
+
+function showTip(category_index, tip_index) {
+  var target = document.getElementById("app");
+  target.innerHTML = "";
+
+  var html_string = "";
+  html_string += `
+    <info>
+      <div class="ml-2">
+        <h1>${database[category_index].tips[tip_index].title}</h1>
+        <p>${database[category_index].tips[tip_index].content}</p>
+      </div>
+    </info>
+  `;
+
+  target.innerHTML = html_string;
+  toggleBackButton(true);
+  setBackButtonTarget("loadCategory(" + category_index + ")");
 }
 
 loadCategories();
